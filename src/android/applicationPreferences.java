8f58e54c8965ec3e74c89bedf23142d878fbf1ff
David Ferrell <dferrell@windstream.net>
2013-09-30 13:38:15 -0400

    state

--- src/android/applicationPreferences.java
+++ src/android/applicationPreferences.java
@@ -1,10 +1,9 @@
-package com.appgiraffe.plugins.applicationPreferences;
+package com.appgiraffe.plugins.preferences;
 
 import java.util.Iterator;
 import java.util.Map;
 
 import org.apache.cordova.CordovaPlugin;
-//import org.apache.cordova.PluginResult;
 import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -27,10 +26,7 @@ public class applicationPreferences extends CordovaPlugin {
     public boolean execute(String action, JSONArray args,
 			CallbackContext callbackContext) throws JSONException {
     	
-        //PluginResult.Status status = PluginResult.Status.OK;
         String result = "";
-
-        //SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this.ctx.getActivity());
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(cordova.getActivity());
         
         try {
@@ -39,11 +35,9 @@ public class applicationPreferences extends CordovaPlugin {
                 if (sharedPrefs.contains(key)) {
                     Object obj = sharedPrefs.getAll().get(key);
                     
-                    //return new PluginResult(status, obj.toString());
                     callbackContext.success(obj.toString());
                     return true;
                 } else {
-                    //return createErrorObj(NO_PROPERTY, "No such property called " + key);
                 	callbackContext.error("No such property called " + key);
                 	return false;
                 }
@@ -57,12 +51,10 @@ public class applicationPreferences extends CordovaPlugin {
                     } else {
                         editor.putString(key, value);
                     }
-                    //return new PluginResult(status, editor.commit());
                     editor.commit();
                     callbackContext.success();
                     return true;
                 } else {
-                    //return createErrorObj(NO_PROPERTY, "No such property called " + key);
                 	callbackContext.error("No such property called " + key);
                 	return false;
                 }
@@ -74,39 +66,25 @@ public class applicationPreferences extends CordovaPlugin {
                     Map.Entry <?,?>pairs = (Map.Entry<?,?>)it.next();
                     obj.put(pairs.getKey().toString(), pairs.getValue().toString());
                 }
-                //return new PluginResult(status, obj);
                 callbackContext.success(obj);
                 return true;
             } else if (action.equals("show")) {
                 String activityName = args.getString(0);
                 Intent intent = new Intent(Intent.ACTION_VIEW);
-                //intent.setClassName(this.ctx.getContext(), activityName);
                 intent.setClassName(cordova.getActivity(), activityName);
                 try {
-                    //this.ctx.startActivity(intent);
                 	cordova.startActivityForResult(this, intent, 0);
                 } catch (ActivityNotFoundException e) {
-                    //return createErrorObj(NO_PREFERENCE_ACTIVITY, "No preferences activity called " + activityName);
                 	callbackContext.error("No preferences activity called " + activityName);
                 	return false;
                 }
             } 
         } catch (JSONException e) {
-            //status = PluginResult.Status.JSON_EXCEPTION;
         	callbackContext.error(e.getMessage());
         	return false;
         }
-        //return new PluginResult(status, result);
         callbackContext.success(result);
 		return true;
     }
-
-    /**
-    private PluginResult createErrorObj(int code, String message) throws JSONException {
-        JSONObject errorObj = new JSONObject();
-        errorObj.put("code", code);
-        errorObj.put("message", message);
-        return new PluginResult(PluginResult.Status.ERROR, errorObj);
-    }**/
 
 }
